38633a1bd5885f32c9144093071ec579c0eb0e46
manuel <manuelquiroz2332@gmail.com>
2023-04-13 12:07:58 -0700

    Inicio de Registro en base de datos

--- PHP/registro.php
+++ PHP/registro.php
@@ -5,7 +5,7 @@
          <div class="wrapper-main">
           <section class="section-default">
             <h1>Registrarse</h1>
-            <form action="Includes/registro.inc.php" method="post">
+            <form class="from-sginup" action="Includes/registro.inc.php" method="post">
                 <input type="text" name="uid" placeholder="Nombre de Usuario">
                 <input type="text" name="mail" placeholder="Correo Electronico">
                 <input type="password" name="pwd" placeholder="Password">
