be15339362b41408131ae3411aad215ee280c35e
manuel <manuelquiroz2332@gmail.com>
2023-04-13 11:25:24 -0700

    Carga de PHP

---
+++ PHP/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <nav>
+            <a href="#">
+                <img src="Images\logo.jpeg" alt="logo">
+            </a>
+            <ul>
+                <li><a href="#">Pagina Principal</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Ubicacion</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+            <div>
+                <form action="Includes/login.inc.php" method="post">
+                    <input type="text" name="mailuid" placeholder="Username/E-Mail">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <button type="submit" name=login-submit>Login</button>
+                </form>
+                <a href="IniciarSesion.php">Iniciar Sesion</a>
+                <form action="Includes/logout.inc.php" method="post">
+                    <button type="submit" name=logout-submit>Cerrar sesion</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+    
+</body>
+</html>
---
+++ PHP/pagprincipalphp.php
@@ -0,0 +1,10 @@
+<?php  
+    require "header.php"
+?>
+        <main>
+            <p>Cerraste Sesion</p>
+            <p>Iniciaste Sesion</p>
+        </main>
+<?php 
+    require "footer.php"
+?>
