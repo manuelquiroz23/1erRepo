08dfcb64f54cbe9ecafa9b9cba3a949d7c23a06e
manuelquiroz23 <39072973+manuelquiroz23@users.noreply.github.com>
2023-04-25 07:29:00 -0700

    asignacion de credenciales para hosting

--- PHP/Includes/dbh.inc.php
+++ PHP/Includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$dbUsername = "root";
-$dBPassword = "";
-$dbName = "PlanchaduriaLomaLinda";
+$dbUsername = "id20635539_planchadurialomalinda";
+$dBPassword = "Z2z0P@sn-=?37S";
+$dbName = "id20635539_dbplanchaduria";
 
 $conn = mysqli_connect($servername,$dbUsername,$dBPassword,$dbName);
 
--- PHP/Includes/login.inc.php
+++ PHP/Includes/login.inc.php
@@ -12,21 +12,20 @@ if(isset($_POST['login-submit'])){
         exit(); 
     }
     else {
-        $sql = "SELECT * FROM clientes WHERE uIdClientes=? OR CorreoCliente=?";
-        echo($sql);
+        $sql = "SELECT * FROM clientes WHERE uIdClientes = ? OR CorreoCliente = ?";
         $stmt = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt, $sql)) {
-                header("Location: ../index.php?error=sqlerror");
+            header("Location: ../index.php?error=sqlerror");
             exit(); 
         }
         else {
-            mysqli_stmt_bind_param($stmt, "ss", $mailuid, $password);
+            mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
             
             if($row = mysqli_fetch_assoc($result)){
                 $pwdCheck = password_verify($password, $row['PasswordCliente']);
-                echo $row['PasswordCliente'];
+                
                 if($pwdCheck == false){ 
                     header("Location: ../index.php?error=wrongpwd");
                     exit(); 
@@ -34,18 +33,18 @@ if(isset($_POST['login-submit'])){
                 else if($pwdCheck == true){
                     session_start();
                     $_SESSION['ClienteId'] = $row['Id_Cliente'];
-                    $_SESSION['ClienteuId'] = $row['uIdCliente'];
+                    $_SESSION['ClienteuId'] = $row['uIdClientes'];
 
                     header("Location: ../pagprincipal.php?login=success");
                     exit(); 
                 }
                 else {
-                    header("Location: ../pagprincipal.php?error=wrongpwd");
+                    header("Location: ../index.php?error=wrongpwd");
                     exit(); 
                 }
             }
             else {
-                header("Location: ../pagprincipal.php?error=nouser");
+                header("Location: ../index.php?error=nouser");
                 exit(); 
             }
 
