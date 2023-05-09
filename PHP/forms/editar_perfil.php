1fde82eb879805f86a2e2f91dd638472329f5697
manuel <manuelquiroz2332@gmail.com>
2023-05-09 15:11:01 -0700

    edicion de perfil

--- PHP/forms/editar_perfil.php
+++ PHP/forms/editar_perfil.php
@@ -1,3 +1,35 @@
 <?php 
-var_dump($_POST)
+session_start();
+
+// Establecer la conexión a la base de datos
+$servername = "localhost";
+$dbUsername = "root";
+$dBPassword = "";
+$dbName = "PlanchaduriaLomaLinda";
+
+$conn = mysqli_connect($servername,$dbUsername,$dBPassword,$dbName);
+
+include('../Includes/dbh.inc.php');
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$clave = $_POST['clave'];
+$IdUsuario = $_SESSION['ClienteId'];
+
+
+$c = <<<SQL
+
+UPDATE
+    clientes
+SET
+    uIdClientes=NULLIF( '$nombre' , '' ),
+    CorreoCliente='$email',
+    PasswordCliente='$clave'
+WHERE Id_Cliente='$IdUsuario'
+LIMIT 1
+SQL;
+
+mysqli_query($conn, $c);
+header("Location: ../perfil.php?");
+var_dump($_POST);
 ?> 
--- PHP/perfil.php
+++ PHP/perfil.php
@@ -16,23 +16,22 @@ $a = mysqli_fetch_assoc($fDatos);
 
 var_dump($a);
 ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/designperfil.css">
+</head>
 <h1>Modificar tus datos</h1>
 <form id="datos" method="post" action="forms/editar_perfil.php">
     <div>
         <div>Nombre<span></span><input type="text" value="<?php echo $a['uIdClientes'];?>" name="nombre" /></div>
         <div>E-Mail<span></span><input type="email" value="<?php echo $a['CorreoCliente'];?>" name="email"/></div> 
         <div class="profile">
-            <span>Tu perfil actual</span>
-            <div><img src="PHP\Images\<?php echo $a['imagen'];?>" width="160" alt="Foto actual"/></div>
-            <div class="image_actions">
-                <input type="file" id="new_image" name="foto"/>
-                <label for="new_image">Usar imagen nueva</label>
-
-                <input type="checkbox" id="del_image" name="foto_eliminar"/>
-                <label for="del_image">Eliminar foto actual</label>
-            </div>
             <div><span>Actualizar Password</span><input type="password" name="clave"></div>
         </div>
     </div>
     <div><input class="aligned" type="submit" value="Guardar cambios" /></div>
+    <a href="indexcliente.php">Regresar</a>
 </form>
